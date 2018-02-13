3fe075a125c2e1f2bb5a23e7e7e64384dd30ef10
Van <vannio@users.noreply.github.com>
2018-02-13 18:28:33 +0000

    JSONify the create method

--- funcs/create.js
+++ funcs/create.js
@@ -3,26 +3,25 @@
 const AWS = require('aws-sdk');
 AWS.config.setPromisesDependency(Promise);
 
-const querystring = require('querystring');
 const path = require('path');
 const crypto = require('crypto');
 const tableName = process.env.DDB_Table;
 const documentClient = new AWS.DynamoDB.DocumentClient();
-
-const render = (link, submitted) => `
-<html>
-  <body>
-    <h3><a href="${link}">${link}</a></h3>
-    <p>URL ${submitted} was shortened to: <a href="${link}">${link}</a></p>
-  </body>
-</html>
-`;
+const headers = { 'Content-Type': 'application/json' };
 
 module.exports.handler = (event, context, callback) => {
-  console.log(JSON.stringify(event));
-  const submitted = querystring.parse(event.body).link;
-  const prefix = event.headers.Referer || 'http://vann.io/';
-  console.log(`URL submitted: ${submitted}`);
+  console.log('EVENT', JSON.stringify(event));
+  const body = JSON.parse(event.body);
+  if (!body || !body.url) {
+    console.log(`ERROR No URL submitted`);
+    callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({ message: `No URL submitted` }),
+      headers,
+    });
+  }
+  const url = body.url;
+  const prefix = event.headers.Referer;
   return new Promise((resolve) => {
     resolve(
       crypto.randomBytes(8)
@@ -31,36 +30,39 @@ module.exports.handler = (event, context, callback) => {
         .substring(0, 4)
     );
   })
-    .then(slug => {
-      console.log(`Trying to save URL: ${submitted}, slug: ${slug}`);
-      return documentClient
-        .put({
-          TableName: tableName,
-          Item: {
-            slug,
-            url: submitted,
-          },
-          Expected: {
-            url: { Exists: false },
-          },
-        })
-        .promise()
-        .then(() => slug);
+  .then(slug => (
+    documentClient.put({
+      TableName: tableName,
+      Item: {
+        slug,
+        url,
+      },
+      Expected: {
+        url: { Exists: false },
+      },
+    }))
+    .promise()
+    .then(() => slug)
+  )
+  .then(slug =>
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Success',
+        data: {
+          url,
+          shrink: path.join(prefix, slug).replace(':/', '://')
+        }
+      }),
+      headers,
     })
-    .then(slug => {
-      console.log('Success');
-      const link = path.join(prefix, slug).replace(':/', '://');
-      return callback(null, {
-        statusCode: 200,
-        body: render(link, submitted),
-        headers: { 'Content-Type': 'text/html' },
-      });
-    })
-    .catch(error => {
-      console.log(`Oh no, hit an error! ${error}`);
-      callback(null, {
-        statusCode: 400,
-        body: 'Something went wrong, please try again',
-      });
+  )
+  .catch(error => {
+    console.log(`ERROR ${error}`);
+    callback(null, {
+      statusCode: 500,
+      body: JSON.stringify({ message: error }),
+      headers,
     });
+  });
 };
